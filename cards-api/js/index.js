217782a63cb6d7fdc77db05b9c140c207d7dfae1
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-23 23:46:38 +0600

    details part added

--- cards-api/js/index.js
+++ cards-api/js/index.js
@@ -49,10 +49,32 @@ const cardDisplay = (cards) => {
         <div class="card-body">
           <h5 class="card-title">${card.suit}</h5>
           <p class="card-text">${card.code}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <a onclick = "cardDetails('${card.code}')" href="#" class="btn btn-primary">Details</a>
         </div>
       </div>
         `;
         main.appendChild(div);
     }
+}
+const cardDetails = (code) => {
+    fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
+        .then(res => res.json())
+        .then(data => {
+            const allCards = data.cards;
+            const singleCard = allCards.find(card => card.code === code);
+            const div = document.createElement('div');
+            main.innerHTML = '';
+            div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                <img src="${singleCard.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">${singleCard.suit}</h5>
+                  <p class="card-text">${singleCard.code}</p>
+                  <p class="card-text">${singleCard.value}</p>
+                </div>
+              </div>
+                `;
+                main.appendChild(div);
+
+        });
 }
