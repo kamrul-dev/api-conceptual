fe4ea585089a58257a3bdee8993421f46858ad11
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-23 22:09:00 +0600

    display weight on UI

--- dog-api/js/index.js
+++ dog-api/js/index.js
@@ -12,10 +12,11 @@ const displayDog = (dogList) => {
     for (const dog of first10Data) {
         const div = document.createElement('div');
         div.classList.add("col-lg-4")
-        console.log(dog)
+        console.log(dog.weight.imperial)
         div.innerHTML = `
             <h2>${dog.name}</h2>
             <p>${dog.temperament}</p>
+            <h4>${dog.weight.imperial}</h4>
             <img width = "400px" height = "250px" src = "${dog.image.url}">
         `;
         main.appendChild(div);
