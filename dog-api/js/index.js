c58ced7ae80ae5bf579cc618ce6dbc1501a7ee53
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-23 22:06:58 +0600

    display dog name, temparament, image on uI

--- dog-api/js/index.js
+++ dog-api/js/index.js
@@ -5,7 +5,19 @@ const loadDog = () => {
         .then(data => displayDog(data));
 }
 
-// display dog data
+// display dog data on UI
 const displayDog = (dogList) => {
-    console.log(dogList)
+    const main = document.getElementById('main');
+    const first10Data = dogList.slice(0, 10);
+    for (const dog of first10Data) {
+        const div = document.createElement('div');
+        div.classList.add("col-lg-4")
+        console.log(dog)
+        div.innerHTML = `
+            <h2>${dog.name}</h2>
+            <p>${dog.temperament}</p>
+            <img width = "400px" height = "250px" src = "${dog.image.url}">
+        `;
+        main.appendChild(div);
+    }
 }
