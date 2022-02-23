3efb36804f324f36de22d2272d71a8cef545905e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-23 22:40:36 +0600

    add event handler and get input value

---
+++ cards-api/js/index.js
@@ -0,0 +1,10 @@
+// load card api https://deckofcardsapi.com/api/deck/new/draw/?count=2
+
+// step 1: add button event handler
+// step 2: get input value
+
+const searchButton = () => {
+    const input = document.getElementById('input-value');
+    const inputValue = input.value;
+    console.log(inputValue) 
+}
