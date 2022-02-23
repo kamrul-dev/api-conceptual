ffe235c5f4ee40cf60a2cce88cc3e908d898ee64
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-23 23:30:19 +0600

    display card on UI

--- cards-api/js/index.js
+++ cards-api/js/index.js
@@ -4,6 +4,8 @@
 // step 2: get input value
 // error handling for string value
 // error handling for empty input field
+const main = document.getElementById('main');
+
 
 const searchButton = () => {
     const input = document.getElementById('input-value');
@@ -14,21 +16,43 @@ const searchButton = () => {
         // alert('please enter number')
         error.innerText = "*pleae enter a number !";
         input.value = '';
+        main.innerHTML = '';
     }
-    else if(inputValue <=0){
+    else if (inputValue <= 0) {
         error.innerText = "Please enter a positive number !";
-        input.value = '';   
+        input.value = '';
+        main.innerHTML = '';
     }
-    else{
+    else {
+        main.innerHTML = '';
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
-        .then(res => res.json())
-        .then(data => cardDisplay(data));
+            .then(res => res.json())
+            .then(data => cardDisplay(data.cards));
 
-        input.value = ''; 
+        input.value = '';
+        error.innerHTML = '';
     }
-    
+
 }
 
-const cardDisplay = (cards) =>{
-    console.log(cards)
+// display card 
+const cardDisplay = (cards) => {
+    // cards = cards.cards 
+    for (const card of cards) {
+        console.log(card.image)
+        const div = document.createElement('div');
+        div.classList.add("col-lg-4");
+        div.classList.add("mb-5");
+        div.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${card.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${card.suit}</h5>
+          <p class="card-text">${card.code}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+        `;
+        main.appendChild(div);
+    }
 }
