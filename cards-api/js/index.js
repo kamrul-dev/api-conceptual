026eb30a73267b1d00fbf775b39158880d19e2ef
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-23 22:52:21 +0600

    error handling for empty input field

--- cards-api/js/index.js
+++ cards-api/js/index.js
@@ -3,14 +3,16 @@
 // step 1: add button event handler
 // step 2: get input value
 // error handling for string value
+// error handling for empty input field
 
 const searchButton = () => {
     const input = document.getElementById('input-value');
     const error = document.getElementById('error');
     const inputValue = input.value;
     //isNan check number or string/others. if string / others then enter this block
-    if (isNaN(inputValue)) {
+    if (isNaN(inputValue) || inputValue == '') {
         // alert('please enter number')
         error.innerText = "*pleae enter a number !";
     }
+    
 }
